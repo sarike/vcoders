24b875dc87196e089040c63a85d46c9e4846d52c
sarike <sarike@timefly.cn>
2018-05-09 23:08:23 +0800

    fix: add code source and server link

--- app/container/footer.jsx
+++ app/container/footer.jsx
@@ -7,7 +7,16 @@ class Footer extends PureComponent {
             <div className="footer p-4">
                 <div className="container">
                     <div className="links">
-                        <a href="#">源码</a>
+                        <a
+                            href="https://github.com/sarike/vcoders"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >源码</a>
+                        <a
+                            href="https://m.do.co/c/a6759cf25bea"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                        >本站服务器</a>
                     </div>
                     <div>
                         <small>VCoders（我们程序员） - 一个正经的程序员扯淡社区。</small>
